753476ca11f0f81aa6340b97167057473f4f2b54
mahsum arik <mahsumarik311@gmail.com>
2024-05-12 00:16:21 +0300

    Mahsum ekledi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+'use client'
+import Nav from "./components/Navbar"
+import Footer from "./components/Footer"
+import FormHeader from "./components/FormHeader";
+import SuccessNotification from "./components/SuccessNotification";
+import UnsuccessNotification from "./components/UnsuccessfulNotification";
 
 export default function Home() {
   return (
     <main className="">
-      
+      <Nav />
+      <FormHeader />
+      {/* < SuccessNotification /> */}
+      <UnsuccessNotification/>
+      <Footer/>
     </main>
   );
 }
