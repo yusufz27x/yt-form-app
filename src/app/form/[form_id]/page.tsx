59e4e24e2d6076223282435ae510a7a781bf3a34
Yusuf Zeybek <yusufz27x@gmail.com>
2024-05-12 13:13:25 +0300

    Added form header

--- src/app/form/[form_id]/page.tsx
+++ src/app/form/[form_id]/page.tsx
@@ -1,10 +1,11 @@
 "use client"
+import FormHeader from '@/app/components/FormHeader';
 import React from 'react'
 
 const Form = () => {
   return (
     <div>
-        
+        <FormHeader/>
     </div>
   )
 }
