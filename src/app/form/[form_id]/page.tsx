a0573cff3562f9a6c4a93e09d5269ea074da0679
Yusuf Zeybek <yusufz27x@gmail.com>
2024-05-12 15:13:25 +0300

    Fetch form by id using api

--- src/app/form/[form_id]/page.tsx
+++ src/app/form/[form_id]/page.tsx
@@ -2,12 +2,27 @@
 import FormHeader from '@/app/components/FormHeader';
 import React from 'react'
 
-const Form = () => {
-	return (
-		<div>
-			<FormHeader />
-		</div>
-	)
+const getForm = async () => {
+	try {
+		const result = await fetch("http://localhost:3000/api/form/[formId]", { cache: "no-store", });
+
+		if (!result.ok) {
+			throw new Error('Failed to fetch form');
+		}
+
+	} catch (error) { console.log("Error loading form", error); }
 }
 
-export default Form;
+export default async function Form() {
+
+	const { form } = await getForm();
+
+	return (
+		<div className='mx-4'>
+			<FormHeader />
+			<div>
+				{form}
+			</div>
+		</div>
+	);
+}
