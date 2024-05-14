bcb9ed1a65984aa3ddd66c4bb0801944ff9f63d3
Yusuf Zeybek <yusufz27x@gmail.com>
2024-05-14 23:14:32 +0300

    mümtaz commit

--- src/app/components/formInputs/FullName.tsx
+++ src/app/components/formInputs/FullName.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FullName = () => {
+  const [fullName, setFullName] = useState('')
+  const [fullNameErrorMessage, setFullNameErrorMessage] = useState('')
+
+  const handleFullNameChange = (e: { target: { value: React.SetStateAction<string> } }) => {
+    setFullName(e.target.value)
+  }
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    const words = fullName.trim().split(/\s+/)
+    if (words.length < 2) {
+      setFullNameErrorMessage('Please enter your full name (at least two words).')
+    } else {
+      console.log('Name submitted:', fullName)
+    }
+  }
+
   return (
-    <div>FullName</div>
+    <div className='flex justify-center items-center bg-slate-300 rounded-3xl my-2 p-6'>
+      <div className='w-full max-w-lg'>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-1'>
+            <label htmlFor='fullName' className='text-slate-600'>
+              Full Name:
+            </label>
+            <input
+              id='fullName'
+              type='text'
+              placeholder='Enter your full name'
+              className='w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-black'
+              value={fullName}
+              onChange={handleFullNameChange}
+            />
+            {fullNameErrorMessage && <div style={{ color: 'red' }}>{fullNameErrorMessage}</div>}
+          </div>
+          <button type='submit' className='bg-orange-400 rounded-md px-2 py-1 justify-center '>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/app/components/formInputs/PhoneNumberInput.tsx
+++ src/app/components/formInputs/PhoneNumberInput.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PhoneNumberInput = () => {
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [phoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState('')
+
+  const handlePhoneNumberChange = (e: { target: { value: React.SetStateAction<string> } }) => {
+    setPhoneNumber(e.target.value)
+  }
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    if (phoneNumber.trim()) {
+      console.log('Phone Number submitted:', phoneNumber)
+    }
+  }
+
   return (
-    <div>PhoneNumberInput</div>
+    <div className='flex justify-center items-center bg-slate-300 rounded-3xl my-2 p-6'>
+      <div className='w-full max-w-lg'>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-1'>
+            <label htmlFor='phone' className='text-slate-600'>
+              Phone Number:
+            </label>
+            <input
+              id='phone'
+              type='tel'
+              pattern='0[0-9]{3} [0-9]{3} [0-9]{4}'
+              placeholder='05XX XXX XXXX'
+              className='w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 text-black'
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+            />
+            {phoneNumberErrorMessage && <div style={{ color: 'red' }}>{phoneNumberErrorMessage}</div>}
+          </div>
+          <button type='submit' className='bg-orange-400 rounded-md px-2 py-1 justify-center '>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
