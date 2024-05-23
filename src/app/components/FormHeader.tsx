476518955c3b1d78a2869af4847da05ca33542aa
Yusuf Zeybek <yusufz27x@gmail.com>
2024-05-23 14:44:37 +0300

    Added form name and description to FormHeader

--- src/app/components/FormHeader.tsx
+++ src/app/components/FormHeader.tsx
@@ -1,25 +1,47 @@
 import React, { useState, useEffect } from "react";
 import Logo from "./Logo_3D/Logo";
-import { formData } from "../mock_data"; 
+import { usePathname } from 'next/navigation';
+import { ApiForm } from "@/models/Form";
 
 const FormHeader = () => {
-  const [formTitle, setFormTitle] = useState(""); 
-  const [formDescription, setFormDescription] = useState(""); 
 
-  useEffect(() => {
-    const firstData = formData[0];
+	const [form, setForm] = useState<ApiForm | null>(null);
+	const pathname = usePathname();
 
-    setFormTitle(firstData.name);
-    setFormDescription(firstData.description);
-  }, []); 
+	useEffect(() => {
+		const getForm = async () => {
 
-  return (
-    <div className="mt-12 ">
-      <Logo />
-      <h1 className="text-center mt-2 text-[#f0d1b8] text-xl font-bold">{formTitle}</h1>
-      <p className="text-center text-[#f0d1b8]">{formDescription}</p>
-    </div>
-  );
+			try {
+				const result = await fetch(`http://localhost:3000/api/form/${pathname.split("/").pop()}`, { cache: "no-store" });
+
+				if (!result.ok) {
+					throw new Error('Failed to fetch form');
+				}
+
+				const data = await result.json();
+				setForm(data.form);
+			} catch (error) {
+				console.log("Error loading form", error);
+			}
+		};
+		if (pathname) {
+			getForm();
+		}
+	}, [pathname]);
+
+	return (
+		<div className="mt-12" style={{paddingTop: '5vh'}}>
+			<Logo />
+			{form ? (
+				<>
+					<h1 className="text-center mt-2 text-[#f0d1b8] text-xl font-bold">{form.name}</h1>
+					<p className="text-center text-[#f0d1b8]">{form.description}</p>
+				</>
+			) : (
+				<p className="text-center text-[#f0d1b8]">Loading...</p>
+			)}
+		</div>
+	);
 };
 
 export default FormHeader;
