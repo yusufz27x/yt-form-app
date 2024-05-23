1b3674e7503c0b5f1795b20ae54954650cfaee6f
Yusuf Zeybek <yusufz27x@gmail.com>
2024-05-23 16:20:37 +0300

    Display form according to the data

--- src/app/form/[form_id]/page.tsx
+++ src/app/form/[form_id]/page.tsx
@@ -5,7 +5,15 @@ import React, { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation';
 import SuccessNotification from '@/app/components/SuccessNotification';
 import UnsuccessfulNotification from '@/app/components/UnsuccessfulNotification';
-import { ApiForm } from '@/models/Form';
+import { ApiForm, ApiQuestion } from '@/models/Form';
+import ShortAnswerInput from '@/app/components/formInputs/ShortAnswerInput';
+import EmailInput from '@/app/components/formInputs/EmailInput';
+import FullName from '@/app/components/formInputs/FullName';
+import PhoneNumberInput from '@/app/components/formInputs/PhoneNumberInput';
+import LongAnswerInput from '@/app/components/formInputs/LongAnswerInput';
+import MultipleChoiceInput from '@/app/components/formInputs/MultipleChoiceInput';
+import DepartmentInput from '@/app/components/formInputs/DepartmentNumber';
+import StudentNumberInput from '@/app/components/formInputs/StudentNumber';
 
 export default function Form() {
 
@@ -34,10 +42,58 @@ export default function Form() {
 
 	return (
 		<div className='mx-4'>
-			<FormHeader />
-			<div>
-				{/* {form} */}
+			<div className='m-4'>
+				<FormHeader />
 			</div>
+			{form ? (
+				<div>
+					{form.questions.map((question: ApiQuestion) => (
+						<div className='flex justify-center'>
+							<div className="bg-gray-100 w-1/3 rounded-3xl p-4 m-3 items-center">
+								<p>{question.question}</p>
+								{(() => {
+									switch (question.type) {
+										case 0:
+											return <p>Error displaying question 0.</p>
+											return <ShortAnswerInput />
+											break;
+										case 1:
+											return <EmailInput />
+											break;
+										case 2:
+											return <FullName />
+											break;
+										case 3:
+											return <PhoneNumberInput />
+											break;
+										case 4:
+											return <p>Error displaying question 4.</p>
+											return <LongAnswerInput />
+											break;
+										case 5:
+											return <p>Error displaying question 5.</p>
+											return <MultipleChoiceInput></MultipleChoiceInput>
+											break;
+										case 6:
+											return <p>Error displaying question 6.</p>
+											return <DepartmentInput />
+											break;
+										case 7:
+											return <p>Error displaying question 7.</p>
+											return <StudentNumberInput />
+										default:
+											return <p>Error displaying question.</p>
+									}
+								})()}
+							</div>
+						</div>
+					))}
+				</div>
+			) : (
+				<p className="text-center text-[#f0d1b8]">Loading...</p>
+			)}
+
+
 			{/* TODO: submit button with update API */}
 			{/* <button
 				type="submit"
